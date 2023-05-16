51a9469c8db131ea681adb7bfe46df0c7dbc5194
Preensikl <Preensikl@mail.ru>
2023-05-16 18:15:39 +0300

    Fix

--- app/functions/get_pdf_menu_function.py
+++ app/functions/get_pdf_menu_function.py
@@ -1,18 +1,15 @@
 import os
-
 from telegram import Update
 from telegram.ext import ContextTypes
-
-from config import MENU_MESSAGE    
-
-
-menu_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
-                            )+"\static\menu.pdf"
+from config import MENU_MESSAGE
 
 
 async def get_pdf_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = update.message.from_user.id
-    context.chat_data[user_id] = "menu"
-    with open(menu_path, "rb") as file:
-        await context.bot.send_message(chat_id=user_id, text=MENU_MESSAGE)
-        await context.bot.send_document(chat_id=user_id, document=file)
+    menu_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    menu_path = menu_path+"/static/menu.pdf"
+    chat_id = update.message.from_user.id
+    context.chat_data[chat_id] = "menu"
+    menu_doc = open(menu_path, 'rb')
+    context.bot.send_document(chat_id, menu_doc)
+    await context.bot.send_message(chat_id=chat_id, text=MENU_MESSAGE)
+    await context.bot.send_document(chat_id=chat_id, document=menu_doc)
