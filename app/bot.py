08bedf6fb315d0ad07a23af55c311f005ea12cb0
Preensikl <Preensikl@mail.ru>
2023-05-16 16:13:11 +0300

    create about + add config

--- app/bot.py
+++ app/bot.py
@@ -1,6 +1,6 @@
 import sys
 
-from config import BOT_TOKEN, BUTTONS, HELLO_TEXT
+from config import BOT_TOKEN, BUTTONS, HELLO_TEXT, ABOUT_TEXT
 from filters import BASE_MESSAGE_FILTERS
 from telegram import KeyboardButton, ReplyKeyboardMarkup, Update
 from telegram.ext import (Application, CommandHandler, ContextTypes,
@@ -31,8 +31,9 @@ async def main_menu(update: Update,
                                    reply_markup=keyboard_markup)
 
 
-async def about():
-    pass
+async def about(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_chat.id
+    await context.bot.send_message(chat_id=user_id, text=ABOUT_TEXT)
 
 
 async def agenda():
@@ -55,8 +56,8 @@ async def any_message(update: Update,
                       context: ContextTypes.DEFAULT_TYPE) -> None:
     if context.chat_data:
         pass
-    if update.message.text == BUTTONS['back']:
-        await main_menu(update, context)
+    if update.message.text == BUTTONS['about']:
+        await about(update, context)
 
 
 def check_creds() -> bool:
