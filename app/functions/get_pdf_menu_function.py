5fe4f70011068133a3d3b5653e5995fc51e9fc56
Preensikl <Preensikl@mail.ru>
2023-05-16 18:19:32 +0300

    Fix menu_send

--- app/functions/get_pdf_menu_function.py
+++ app/functions/get_pdf_menu_function.py
@@ -7,8 +7,8 @@ from config import MENU_MESSAGE
 async def get_pdf_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     menu_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     menu_path = menu_path+"/static/menu.pdf"
-    user_id = update.message.from_user.id
-    context.chat_data[user_id] = "menu"
-    with open(menu_path, "rb") as file:
-        await context.bot.send_message(chat_id=user_id, text=MENU_MESSAGE)
-        await context.bot.send_document(chat_id=user_id, document=file)
+    chat_id = update.message.from_user.id
+    context.chat_data[chat_id] = "menu"
+    menu_doc = open(menu_path, 'rb')
+    await context.bot.send_message(chat_id=chat_id, text=MENU_MESSAGE)
+    await context.bot.send_document(chat_id, menu_doc)
