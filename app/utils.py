b27cbafcdf956af8fc313543e0f1e5eb42172b72
Preensikl <Preensikl@mail.ru>
2023-05-18 23:41:03 +0300

    test push

--- app/utils.py
+++ app/utils.py
@@ -11,6 +11,7 @@ async def forward_message(message, channel_id):
 
 async def get_apply(update: Update, context: ContextTypes.DEFAULT_TYPE,
                     question_text: str, **actions: dict):
+    '...'
     user_id = update.message.from_user.id
     users_text = update.message.text
     call = {
