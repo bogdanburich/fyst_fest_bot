d4134bcaf6564d3ea245ed35653267c9a9af368b
Bogdan Burich <bogdan.buritch@yandex.ru>
2023-05-24 19:18:49 +0700

    Update hello text

--- app/texts.py
+++ app/texts.py
@@ -1,4 +1,19 @@
-HELLO = 'Hello\nChoose one:'
+HELLO = '''
+🎉 Welcome to the FYST FEST bot! 🎊
+
+We're stoked to have you here for our upcoming first-ever festival! This
+chatbot is your ultimate guide to ensure you don't miss a beat. We've got an
+agenda on mind-blowing events lined up just for you.
+
+To dive right in, click the *"Agenda"* button and uncover the insider scoop on
+event schedules.
+
+Whether you're craving electrifying performances, artist insights, or seeking
+thrilling activities - this bot is gonna be your best friend.
+
+More mind-blowing info is heading your way soon. Stay tuned and get ready to
+rock FYST FEST like never before! 🤘🔥
+'''
 
 AGENDA = 'Agenda'
 
