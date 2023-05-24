77b2f71c01e5c10e38b46683b562625e8f5fd3b0
Bogdan Burich <bogdan.buritch@yandex.ru>
2023-05-24 19:23:54 +0700

    Update hello text

--- app/texts.py
+++ app/texts.py
@@ -5,8 +5,8 @@ We're stoked to have you here for our upcoming first-ever festival! This
 chatbot is your ultimate guide to ensure you don't miss a beat. We've got an
 agenda on mind-blowing events lined up just for you.
 
-To dive right in, click the *"Agenda"* button and uncover the insider scoop on
-event schedules.
+To dive right in, click the <b>"Agenda"</b> button and uncover the insider
+scoop on event schedules.
 
 Whether you're craving electrifying performances, artist insights, or seeking
 thrilling activities - this bot is gonna be your best friend.
